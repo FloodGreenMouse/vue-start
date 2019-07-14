232bc718d1f1025bd889132ef715629f9d0f8321
Sergey Platitsin <greenfloodmouse@gmail.com>
2019-07-14 15:31:54 +0300

    init

---
+++ src/components/simple-component/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.simple-component
+  h2 And this is a component
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.page.home
+  h1 This is a home page
+  simpleComponent
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import simpleComponent from '../components/simple-component'
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    simpleComponent
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
